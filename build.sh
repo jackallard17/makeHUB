ae5a666ed5b6ec9b5d8d540c00ca08e227a5eefb
Jack <jackallard17@gmail.com>
2020-01-15 19:51:19 -0600

    reconfiguring submodules

--- build.sh
+++ build.sh
@@ -14,11 +14,10 @@ sudo apt update
 echo -e "${RED} ==makeHUB: CHECKING AND INSTALLING PREBUILT DEPENDENCIES== ${ENDCOLOR}"
 sudo apt-get install $PREBUILT_PACKAGES
 
-#install scratch for arduino
+#install arduino
 echo -e "${RED} ==makeHUB: INSTALLING AND CONFIGURING ARDUINO== ${ENDCOLOR}"
-cd packages/arduino-1.8.10
+cd packages/Arduino
 sudo ./install.sh
-curl https://github.com/bromagosa/Snap4Arduino/releases/download/5.1.0/Snap4Arduino_desktop-gnu-64_5.1.0.tar.gz --output ardunio_snap.tar.gz
 
 #install octoprint
 
