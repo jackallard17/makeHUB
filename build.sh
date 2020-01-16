c6cf97f4650c9c0b2ed64e751b49065f5415b14a
Jack <jackallard17@gmail.com>
2020-01-15 22:12:41 -0600

    build.sh update

--- build.sh
+++ build.sh
@@ -17,6 +17,9 @@ sudo apt update
 echo -e "${RED} ==makeHUB: CHECKING AND INSTALLING PREBUILT DEPENDENCIES== ${ENDCOLOR}"
 sudo apt-get install $PREBUILT_PACKAGES
 
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 #install arduino
 echo -e "${RED} ==makeHUB: INSTALLING AND CONFIGURING ARDUINO== ${ENDCOLOR}"
 cd packages/arduino-1.8.10
